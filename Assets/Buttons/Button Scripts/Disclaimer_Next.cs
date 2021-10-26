24197e0979245c731af4ea9f397c0cbfe37b6099
Nicholas Meyer <46453785+nickgm7@users.noreply.github.com>
2021-10-26 10:45:16 -0700

    Added Next Button and Disclaimer Scene
    Add Disclaimer Scene
    Add next to Disclaimer Scene
    Add next to Start Page

---
+++ Assets/Buttons/Button Scripts/Disclaimer_Next.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Disclaimer_Next : MonoBehaviour
+{    public void OnButtonPress()
+    {
+        SceneManager.LoadScene("Start Page");
+    }
+}
