c9702fd167ce4490714877afe2392519eb09f2f3
lmerbeth <lmerbeth@asu.edu>
2021-10-26 14:48:35 -0700

    Fixed button functionality

--- Assets/Buttons/Button Scripts/Background_Next.cs
+++ Assets/Buttons/Button Scripts/Background_Next.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Disclaimer_Next : MonoBehaviour
+public class Background_Next : MonoBehaviour
 {    public void OnButtonPress()
     {
         SceneManager.LoadScene("Start Page");
