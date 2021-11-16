4407e27978e70fdc884e224e10620ee508bcf57b
alidback <alidback@asu.edu>
2021-11-16 15:34:59 -0700

    animation
    added astronaut run, jump, and idle animation.

--- Assets/Player/Player Script/PlayerMovement.cs
+++ Assets/Player/Player Script/PlayerMovement.cs
@@ -7,12 +7,19 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController2D controller;
 
+    private Animator anim;
 
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
     bool jump;
     float verticalMove = 0f;
+
+    void Awake()
+    {
+        //grab references for animator from game object
+        anim = GetComponent<Animator>();
+    }
     
     // Update is called once per frame
     void Update()
@@ -22,8 +29,13 @@ public class PlayerMovement : MonoBehaviour
         if (verticalMove > 0)
             jump = true;
         else
-            jump = false; 
-        
+            jump = false;
+
+
+        //animation parameters
+        anim.SetBool("run", controller.getGround() == true && horizontalMove != 0);
+        anim.SetBool("jumpAnimation", controller.getGround() == false);
+        anim.SetBool("ground", controller.getGround() && horizontalMove != 0);
     }
 
     void FixedUpdate()
