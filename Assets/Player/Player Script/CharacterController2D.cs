5f7755228b68a1d15b8e12fdac8f2b123a1f24d0
lmerbeth <lmerbeth@asu.edu>
2021-11-18 15:30:09 -0700

    Fixed friction with platforms, player jumping being inconsistent

--- Assets/Player/Player Script/CharacterController2D.cs
+++ Assets/Player/Player Script/CharacterController2D.cs
@@ -134,6 +134,7 @@ public class CharacterController2D : MonoBehaviour
         // If the player should jump...
         if (m_Grounded && jump)
         {
+            m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, 0f);
             // Add a vertical force to the player.
             m_Grounded = false;
             m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
