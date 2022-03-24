337b01a942b24239244582e5afe902b2277e5b5c
GSswenso <72363582+GSswenso@users.noreply.github.com>
2022-03-24 15:26:24 -0700

    Falling Platform Animation

--- Assets/Gimmicks/CrumblingPlatforms/CrumblingScript.cs
+++ Assets/Gimmicks/CrumblingPlatforms/CrumblingScript.cs
@@ -7,11 +7,14 @@ public class CrumblingScript : MonoBehaviour
     Vector3 originalPosition;
     Rigidbody2D rb;
 
+    Animator crumb;
+
     // Start is called before the first frame update
     void Start()
     {
         originalPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
         rb = GetComponent<Rigidbody2D>();
+        crumb = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,6 +22,7 @@ public class CrumblingScript : MonoBehaviour
     {
         if (col.gameObject.name.Equals("Player"))
         {
+            
             Invoke("DropPlatform", 0.3f);
             StartCoroutine(respawn());
             //Destroy(gameObject, 10f);
@@ -33,10 +37,12 @@ public class CrumblingScript : MonoBehaviour
 
     private IEnumerator respawn()
     {
+        crumb.SetBool("isFalling", true);
         yield return new WaitForSeconds(5);
+        crumb.SetBool("isFalling", false);
         rb.velocity = Vector3.zero;
         rb.isKinematic = true;
         gameObject.transform.position = originalPosition;
-        //Debug.Log("test");
+        Debug.Log("test");
     }
 }
